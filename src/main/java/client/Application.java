6c85d8cd6a83b3c87166afc1bc5f7a44e4c41886
Paul Frolke <p.r.frolke@student.tudelft.nl>
2019-02-25 15:28:03 +0100

    added a test client

--- src/main/java/client/Application.java
+++ src/main/java/client/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
     public static void main(String args[]) {
         RestTemplate restTemplate = new RestTemplate();
-        String quote = restTemplate.getForObject("https://shrouded-tor-91595.herokuapp.com/", String.class);
+        String quote = restTemplate.getForObject("https://fast-meadow-28971.herokuapp.com/", String.class);
         System.out.println(quote);
     }
 
