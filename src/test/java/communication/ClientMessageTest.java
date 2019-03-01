126993df464fb4e48c9ebb744cf258a0993869d4
Paul Frolke <p.r.frolke@student.tudelft.nl>
2019-03-01 13:28:44 +0100

    add test classes for current classes

---
+++ src/test/java/communication/ClientMessageTest.java
@@ -0,0 +1,12 @@
+package communication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClientMessageTest {
+    @Test
+    public void testBasic() {
+        assertEquals(1, 1);
+    }
+}
