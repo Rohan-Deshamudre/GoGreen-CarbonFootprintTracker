41814af0132b985b7e2d40caba52f3296de40f40
Paul Frolke <p.r.frolke@student.tudelft.nl>
2019-03-01 17:00:15 +0100

    constrain mirror and status request handlers to json

--- src/main/java/server/MirrorController.java
+++ src/main/java/server/MirrorController.java
@@ -2,6 +2,7 @@ package server;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,8 @@ public class MirrorController {
     /**
      * Adds a page /test/status which responds an up message
      */
-    @RequestMapping("/test/status")
+    @RequestMapping(value = "/test/status",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
     public ResponseEntity<String> status() {
         return new ResponseEntity<>("{\"status\" : \"UP\"}", HttpStatus.OK);
@@ -22,7 +24,9 @@ public class MirrorController {
     /**
      * Adds a page /test/mirror which responds by mirroring the request
      */
-    @RequestMapping("/test/mirror")
+    @RequestMapping(value = "/test/mirror",
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
     public ResponseEntity<String> mirror(@RequestBody String req) {
         return new ResponseEntity<>(req, HttpStatus.OK);
