0ab70506e4f3c9ec14426392a99d0aac1a2afe42
Denys <d.o.sheremet@student.tudelft.nl>
2019-03-03 12:43:23 +0100

    ClientApplication has the method sendLoginRequest, which sends a POST request to the server with a LoginRequest and returns true if the server says that the username and password are valid.

--- src/main/java/client/ClientApplication.java
+++ src/main/java/client/ClientApplication.java
@@ -1,16 +1,23 @@
 package client;
 
+import communication.LoginRequest;
+import communication.LoginResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 
 public class ClientApplication {
 
-    private final static String URL = "https://fast-meadow-28971.herokuapp.com/";
+    private final static String URL = "http://localhost:8080/";
 
-    public static void main(String args[]) {
-        getRequestHeroku();
+    public static void main(String args[]) throws URISyntaxException {
+//        getRequestHeroku();
+        boolean success = sendLoginRequest("Roy", "Donders");
+        System.out.println("Success: " + success);
     }
 
     /**
@@ -21,6 +28,20 @@ public class ClientApplication {
         RestTemplate restTemplate = new RestTemplate();
         String quote = restTemplate.getForObject(URL, String.class);
         return quote;
+    }
+
+    public static boolean sendLoginRequest(String username, String password) throws URISyntaxException {
+        LoginRequest req = new LoginRequest(username, password);
+
+        RestTemplate restTemplate = new RestTemplate();
+        URI uri = new URI(URL + "login");
+
+        LoginResponse res = restTemplate.postForObject(uri, req, LoginResponse.class);
+
+        System.out.println();
+        System.out.println(res);
+
+        return res.getSuccess();
     }
 
 }
