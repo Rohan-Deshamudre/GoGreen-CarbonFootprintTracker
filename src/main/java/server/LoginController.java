c3a0f7bccc613917654e52d3f7879b32094d41f9
Paul Frolke <p.r.frolke@student.tudelft.nl>
2019-03-03 16:44:39 +0100

    Move the /login controller to a seperate class

--- src/main/java/server/LoginController.java
+++ src/main/java/server/LoginController.java
@@ -13,10 +13,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import static server.ServerApplication.checkLoginData;
 
 @Controller
-public class HomeController {
-    @RequestMapping("/")
+public class LoginController {
+    /**
+     * Adds a page /login which handles responding to login requests
+     */
+    @RequestMapping(value = "/login",
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
-    String home() {
-        return "Howdy client!";
+    public ResponseEntity<LoginResponse> handleLoginRequest(@RequestBody LoginRequest req) {
+        boolean success = checkLoginData(req);
+        LoginResponse res = new LoginResponse(success);
+        return new ResponseEntity<LoginResponse>(res, HttpStatus.OK);
     }
 }
