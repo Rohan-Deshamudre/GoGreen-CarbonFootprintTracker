a1d8314eb00f03159504abf0bd6ed188745fcd12
Nathan <n.p.a.kindt@student.tudelft.nl>
2019-03-04 14:36:46 +0100

    Added Login Controller Tests

---
+++ src/test/java/server/LoginControllerTest.java
@@ -0,0 +1,39 @@
+package server;
+
+import communication.LoginRequest;
+import communication.LoginResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LoginControllerTest {
+    public static ResponseEntity<String> responseBuilder(String res){
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @Test
+    public void statusTest() {
+        LoginRequest req = new LoginRequest("user", "pwd");
+        LoginController mc = new LoginController();
+        LoginResponse res = new LoginResponse(false);
+        assertEquals(mc.handleLoginRequest(req).toString(), "<200 OK OK,<LoginResponse{\n" +
+                "    success: false\n" +
+                "}>,[]>");
+
+        LoginRequest req1 = new LoginRequest("John", "Wick");
+        LoginController mc1 = new LoginController();
+        LoginResponse res1 = new LoginResponse(true);
+        assertEquals(mc.handleLoginRequest(req1).toString(), "<200 OK OK,<LoginResponse{\n" +
+                "    success: true\n" +
+                "}>,[]>");
+    }
+
+    @Test
+    public void mirrorTest() {
+        MirrorController mc = new MirrorController();
+        assertEquals(mc.mirror("{1231}"), responseBuilder("{1231}"));
+    }
+}
