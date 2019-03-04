d0a08225b98dd8252e170d69c8986ef5cd2d8e8c
Paul Frolke <p.r.frolke@student.tudelft.nl>
2019-03-04 17:23:29 +0100

    Add tests for all testable classes

--- src/test/java/communication/LoginRequestTest.java
+++ src/test/java/communication/LoginRequestTest.java
@@ -14,9 +14,21 @@ class LoginRequestTest {
     }
 
     @Test
+    public void constructorTestEmpty() {
+        LoginRequest req = new LoginRequest();
+        assertNull(req.getUsername());
+        assertNull(req.getPassword());
+    }
+
+    @Test
     public void toStringTest() {
         LoginRequest req = new LoginRequest("user", "pwd");
         assertEquals(req.toString(),"<LoginRequest{\n    username: user\n    password: pwd\n}>");
     }
 
+    @Test
+    public void toStringTestNull() {
+        LoginRequest req = new LoginRequest(null, null);
+        assertEquals(req.toString(), "<LoginRequest{\n    username: \n    password: \n}>");
+    }
 }
