907130267372287afc3fe9bd3730ff7aebb46706
Paul Frolke <p.r.frolke@student.tudelft.nl>
2019-03-06 13:54:35 +0100

    fix LoginRequest.java constructor error

--- src/main/java/communication/ClientMessage.java
+++ src/main/java/communication/ClientMessage.java
@@ -11,6 +11,10 @@ public abstract class ClientMessage extends Message {
         this.password = password;
     }
 
+    public ClientMessage () {
+
+    }
+
     public String getUsername() {
         return username;
     }
