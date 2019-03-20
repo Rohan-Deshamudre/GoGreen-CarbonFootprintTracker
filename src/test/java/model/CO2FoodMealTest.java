966898526f42325ddf655cc65d1519906c2247f5
Justin Sun <sunchuying@gmail.com>
2019-03-20 18:19:46 +0100

    Tests for the CO2FoodMeal class and CO2FoodStore class

--- src/test/java/model/CO2FoodMealTest.java
+++ src/test/java/model/CO2FoodMealTest.java
@@ -13,4 +13,43 @@ public class CO2FoodMealTest {
         assertTrue(meal.getTypeOfMeal().equals("type"));
         assertEquals(meal.getSize(), 2);
     }
+
+    @Test
+    public void toStringTest() {
+        CO2FoodMeal meal = new CO2FoodMeal("username", "type", 1);
+        assertTrue(meal.toString().equals("CO2FoodMeal[musername='username', typeOfMeal='type', size='1']"));
+    }
+
+    @Test
+    public void getMusernameTest() {
+        CO2FoodMeal meal = new CO2FoodMeal("username", "type", 1);
+        assertTrue(meal.getMusername().equals("username"));
+    }
+
+    @Test
+    public void getTypeOfMealTest() {
+        CO2FoodMeal meal = new CO2FoodMeal("username", "type", 1);
+        assertTrue(meal.getTypeOfMeal().equals("type"));
+    }
+
+    @Test
+    public void setTypeOfMealTest() {
+        CO2FoodMeal meal = new CO2FoodMeal("username", "type", 1);
+        meal.setTypeOfMeal("type1");
+        assertTrue(meal.getTypeOfMeal().equals("type1"));
+    }
+
+    @Test
+    public void getSizeTest() {
+        CO2FoodMeal meal = new CO2FoodMeal("username", "type", 1);
+        assertEquals(meal.getSize(), 1);
+    }
+
+    @Test
+    public void setSizeTest() {
+        CO2FoodMeal meal = new CO2FoodMeal("username", "type", 1);
+        meal.setSize(2);
+        assertEquals(meal.getSize(), 2);
+    }
+
 }
