564153d289f7be5a926c6693b70a04cde2d51248
Marvin Evora Sousa <M.EvoraSousa@student.tudelft.nl>
2019-04-01 11:15:58 +0200

    Added the tests for CarbonUtil

---
+++ src/test/java/util/CarbonUtilTest.java
@@ -0,0 +1,40 @@
+package util;
+
+import static junit.framework.TestCase.assertEquals;
+
+import gogreen.application.communication.AddTransportRequest;
+import gogreen.application.util.CarbonUtil;
+import org.junit.jupiter.api.Test;
+
+public class CarbonUtilTest {
+
+    @Test
+    public void getFoodCarbonReductionTest() {
+        assertEquals(CarbonUtil.getFoodCarbonReduction("salad"), 100);
+        assertEquals(CarbonUtil.getFoodCarbonReduction("Vegetarian Meat"), 200);
+        assertEquals(CarbonUtil.getFoodCarbonReduction("Fruit"), 80);
+        assertEquals(CarbonUtil.getFoodCarbonReduction("Else"), 150);
+    }
+
+    @Test
+    public void getLocalProduceCarbonReductionTest() {
+        assertEquals(CarbonUtil.getLocalProduceCarbonReduction(10, true), 23);
+        assertEquals(CarbonUtil.getLocalProduceCarbonReduction(10, false), 17);
+    }
+
+    @Test
+    public void getTransportCarbonReductionTest() {
+        assertEquals(CarbonUtil.getTransportCarbonReduction(AddTransportRequest.TravelType.BIKE, 10), 50);
+        assertEquals(CarbonUtil.getTransportCarbonReduction(AddTransportRequest.TravelType.PUB_TRANSPORT, 10), 20);
+    }
+
+    @Test
+    public void getHomeTempCarbonReductionTest() {
+        assertEquals(CarbonUtil.getHomeTempCarbonReduction(10, 4), 12);
+    }
+
+    @Test
+    public void getSolarPanelCarbonReduction() {
+        assertEquals(CarbonUtil.getSolarPanelCarbonReduction(10, 3), 12);
+    }
+}
