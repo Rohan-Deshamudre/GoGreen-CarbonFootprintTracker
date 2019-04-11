81a951e3ef194c0ddc455a330d7e7c2193207ef5
Denys <d.o.sheremet@student.tudelft.nl>
2019-04-11 13:12:28 +0200

    Fixed failing tests.

--- src/test/java/model/CO2Test.java
+++ src/test/java/model/CO2Test.java
@@ -11,50 +11,50 @@ public class CO2Test {
 
     @Test
     public void getCUsernameTest() {
-        CO2 user = new CO2("user", 10, 11, 12, 13, "101010");
+        CO2 user = new CO2("user", 10, 11, 12, 13, "00000000000000");
         assertTrue(user.getCUsername().equals("user"));
     }
 
     @Test
     public void getCO2FoodTest() {
-        CO2 user = new CO2("user", 10, 11, 12, 13, "101010");
+        CO2 user = new CO2("user", 10, 11, 12, 13, "00000000000000");
         assertEquals(user.getCO2food(), 10);
     }
 
     @Test
     public void getCO2Transport() {
-        CO2 user = new CO2("user", 10, 11, 12, 13, "101010");
+        CO2 user = new CO2("user", 10, 11, 12, 13, "00000000000000");
         assertEquals(user.getCO2transport(), 11);
     }
 
     @Test
     public void getCO2Energy() {
-        CO2 user = new CO2("user", 10, 11, 12, 13, "101010");
+        CO2 user = new CO2("user", 10, 11, 12, 13, "00000000000000");
         assertEquals(user.getCO2energy(), 12);
     }
 
     @Test
     public void getCO2ReducTest() {
-        CO2 user = new CO2("user", 10, 11, 12, 13, "101010");
+        CO2 user = new CO2("user", 10, 11, 12, 13, "00000000000000");
         assertEquals(13, user.getCO2reduc());
     }
 
     @Test
     public void co2ConstructorTest() {
-        CO2 user = new CO2("user", 10, 11, 12, 13, "101010");
+        CO2 user = new CO2("user", 10, 11, 12, 13, "00000000000000");
         assertTrue(user.getCUsername().equals("user"));
         assertEquals(user.getCO2food(), 10);
         assertEquals(user.getCO2transport(), 11);
         assertEquals(user.getCO2energy(), 12);
         assertEquals(user.getCO2reduc(), 13);
-        assertEquals(user.getAchievement(), "101010");
+        assertEquals(user.getAchievement(), "00000000000000");
     }
 
     @Test
     public void toStringTest() {
-        CO2 user = new CO2("user", 10, 11, 12, 13, "101010");
+        CO2 user = new CO2("user", 10, 11, 12, 13, "00000000000000");
         assertEquals(user.toString(), "CO2[cusername='user', Co2Food='10', "
-            + "Co2Transport='11', Co2Energy='12', Co2Reduction='13', achievement='101010']");
+            + "Co2Transport='11', Co2Energy='12', Co2Reduction='13', achievement='00000000000000']");
     }
 }
 
