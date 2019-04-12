f5928fb2ebff6625d72f44ad8db179d047869ef4
Nathan <n.p.a.kindt@student.tudelft.nl>
2019-04-12 12:48:32 +0200

    test client

--- src/test/java/client/ClientApplicationTest.java
+++ src/test/java/client/ClientApplicationTest.java
@@ -1,11 +1,22 @@
 package client;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static gogreen.application.client.ClientApplication.checkAchievement;
+import static gogreen.application.client.ClientApplication.setUser;
+
+import gogreen.application.model.CO2;
 import org.junit.jupiter.api.Test;
 
 class ClientApplicationTest {
-    @Test
-    public void testHeroku() {
 
+    static CO2 user = new CO2("bob", 8, 8, 8, 8, "101010");
+
+    @Test
+    public void checkAchievementTest() {
+        setUser(user);
+        assertTrue(checkAchievement(5));
+        assertFalse(checkAchievement(4));
     }
 
 }
